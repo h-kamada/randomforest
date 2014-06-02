a91af4035f0e9f8c8f62efaa0a2f26045fb1228a
Kamada Hitoshi <h-kamada@jsk.imi.i.u-tokyo.ac.jp>
2014-06-02 21:08:59 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 #define T 1 //サブセットの個数
 using namespace std;
 double a[155], b[155], c[155], d[155];  int e[155];
-int i, j, k, l;
 //Set s;
 //Tree tr;
 //divide_function dv;
@@ -92,20 +91,28 @@ public:
 
 class Tree{
 private:
-  string tree[16], cur;
+  string tree[T][16], cur[T];
+  //  string tree[16], cur;
 public:
   Tree(){
-    for(j=0;j<T;j++){
-      for(i=0;i<16;i++){
-        tree[i]="t";
+    for(int j=0;j<T;j++){
+      for(int i=0;i<16;i++){
+        //        tree[i]="t";
+        tree[j][i]="t";
       }
+      cur[j]="t";
     }
-    cur="t";
+    //    cur="t";
   }
-  string return_last( int i){
-    return tree[tree[i].length()-1];
+  /*  string return_last( int i){
+    return tree[tree[i].length()-1];//これ間違ってないか？？
+    }*/
+  char return_last(int t, int i){
+    char tmp;
+    tmp=tree[t][i].at(tree[t][i].length()-1);
+    return tmp;
   }
-  bool depth_of_cur_is_max (int D){
+  /*  bool depth_of_cur_is_max ( int D){
     if(cur.length()<D){
       cout<<"cur.length"<<cur.length()<<endl;
       return false;
@@ -113,22 +120,46 @@ public:
     else{
       return true;
     }
+    }*/
+  bool depth_of_cur_is_max (int t, int D){
+    if(cur[t].length()<D){
+      cout<<"cur["<<t<<"].length"<<cur[t].length()<<endl;
+      return false;
+    }
+    else{
+      return true;
+    }
   }
-  void state(){
+  /*  void state(){
     Tree::curr();
     for(i=0;i<16;i++){
       Tree::pwd(i);
     }
+    }*/
+  void state(int t){
+    Tree::curr(t);
+    for(int i=0;i<16;i++){
+      Tree::pwd(t, i);
+    }
   }
-  string pwd(int i){
+
+  /*  string pwd(int i){
     cout<<"tree["<<i<<"]:"<<tree[i]<<endl;
     return tree[i];
+    }*/
+  string pwd(int t, int i){
+    cout<<"tree["<<t<<"]["<<i<<"]:"<<tree[t][i]<<endl;
+    return tree[t][i];
   }
-  string curr(){
+  /*  string curr(){
     cout<<"cur: "<<cur<<endl;
     return cur;
+    }*/
+  string curr(int t){
+    cout<<"cur["<<t<<"]: "<<cur[t]<<endl;
+    return cur[t];
   }
-  void next(){
+  /*  void next(){
     for(i=0;i<16;i++){
       if(tree[i].at(tree[i].length()-1)=='e'){}
       else{
@@ -139,35 +170,88 @@ public:
       }
     }
 
+    }*/
+  void next(int t){
+    for(int i=0;i<16;i++){
+      cout<<"a"<<endl;
+      cout<<tree[t][i].length()<<"leng"<<endl;
+      cout<<"t:"<<t<<endl;
+      cout<<cur[t].at(0)<<"wee"<<endl;
+      cout<<"tree's last: "<<tree[t][i].at(tree[t][i].length()-1)<<endl;
+     if(tree[t][i].at(tree[t][i].length()-1)=='e'){cout<<"b"<<endl;}
+      else{
+        cout<<"c"<<endl;
+        cout<<"cur["<<t<<"]:"<<cur[t];;
+        cur[t]=tree[t][i];
+        cout<<"->"<<cur[t]<<endl;
+        return;
+        cout<<"cant look"<<endl;
+      }
+    }
   }
+  /*
   bool next_exist(){
-    if(tree[i].at(tree[i].length()-1)=='t')return true;
-    for(i=0;i<16;i++){
-      if(tree[i].at(tree[i].length()-1)!='e'){
-        cout<<"tree last:"<<tree[i].at(tree[i].length()-1)<<endl;
+  if(tree[i].at(tree[i].length()-1)=='t')return true;//このiって０とかにすべきでは
+  for(i=0;i<16;i++){
+  if(tree[i].at(tree[i].length()-1)!='e'){
+  cout<<"tree last:"<<tree[i].at(tree[i].length()-1)<<endl;
+  return true;
+  }
+  }
+  cout<<"tree last:"<<tree[i].at(tree[i].length()-1)<<endl;
+  return false;
+  }*/
+  bool next_exist(int t){
+    if(tree[t][0].at(tree[t][0].length()-1)=='t')return true;
+    for(int i=0;i<16;i++){
+      if(tree[t][i].at(tree[t][i].length()-1)!='e'){
+        cout<<"tree["<<t<<"]["<<i<<"] isn't at last yet. the last of tree["<<t<<"]["<<i<<"] is"<<tree[t][i].at(tree[t][i].length()-1)<<"  tree["<<t<<"]["<<i<<"]:"<<tree[t][i]<<endl;
         return true;
       }
     }
-    cout<<"tree last:"<<tree[i].at(tree[i].length()-1)<<endl;
+    cout<<"all tree came to last"<<endl;
     return false;
   }
-  void left(int i){
+  /*  void left(int i){
     cout<<"tree["<<i<<"]:"<<tree[i];
     tree[i].append(1, 'l');
     cout<<"->"<<tree[i]<<endl;
+    }*/
+  void left(int t, int i){
+    cout<<"left down:::tree["<<t<<"]["<<i<<"]:"<<tree[t][i];
+    tree[t][i].append(1, 'l');
+    cout<<"->"<<tree[t][i]<<endl;
   }
-  void right(int i){
+  /*  void right(int i){
     cout<<"tree["<<i<<"]:"<<tree[i];
     tree[i].append(1, 'r');
     cout<<"->"<<tree[i]<<endl;
+    }*/
+  void right(int t, int i){
+    cout<<"right down:::tree["<<t<<"]["<<i<<"]:"<<tree[t][i];
+    tree[t][i].append(1, 'r');
+    cout<<"->"<<tree[t][i]<<endl;
   }
-  void end(int i){
+  /*  void end(int i){
     cout<<"tree["<<i<<"]:"<<tree[i];
     tree[i].append(1, 'e');
     cout<<"->"<<tree[i]<<endl;
+    }*/
+  void end(int t, int i){
+    cout<<"tree's last have come:::tree["<<t<<"]["<<i<<"]:"<<tree[t][i];
+    tree[t][i].append(1, 'e');
+    cout<<"->"<<tree[t][i]<<endl;
   }
-  bool check_pwd(int i){
+  /*  bool check_pwd(int i){
     if(tree[i]==cur){
+      return true;
+    }
+    else {
+      return false;
+    }
+    }*/
+  bool check_pwd(int t, int i){
+    if(tree[t][i]==cur[t]){
       return true;
     }
     else {
@@ -185,13 +269,13 @@ public:
     ty=1;
     t=0;//これは仮
     max=0;
-    for(i=0;i<3;i++){
+    for(int i=0;i<3;i++){
       left_[i]=0;
       right_[i]=0;
     }
   }
 
-  bool search(int t){//entropy 0?
+  /*  bool search(int t){//entropy 0?
     int c1,c2,c3;
     c1=0;c2=0;c3=0;
     if(Tree::return_last(1)=="t"){
@@ -214,41 +298,70 @@ public:
     if(c3==0&&c2==0)return true;
     if(c1==0&&c3==0)return true;
     return false;
+    }*/
+  bool search(int t){//entropy 0?
+    //引数　t:t個目のサブセット
+    int c1,c2,c3;
+    c1=0;c2=0;c3=0;
+    if(Tree::return_last(t, 1)=='t'){//"t"->'t'にしたけどあってる？？
+      return false;
+    }
+    for(int i=0;i<16;i++){
+      if(Tree::curr(t)==Tree::pwd(t, i)){
+        if(Set::se_out(t, i)==1){//ここはtか？？
+          c1++;
+        }
+        if(Set::se_out(t, i)==2){
+          c2++;
+        }
+        if(Set::se_out(t, i)==3){
+          c3++;
+        }
+      }
+    }
+    if(c1==0&&c2==0)return true;
+    if(c3==0&&c2==0)return true;
+    if(c1==0&&c3==0)return true;
+    return false;
   }
 
 
 
   void learn(int depth, int K, int ty, int t){
+    //引数 depth:深さ K:関数候補の数 ty:識別関数のタイプ t:サブセットの個数
     depth++;
-    for(j=0;j<t;j++){//TkonoSubset nituite
-      while(Tree::next_exist()){//mattann made itterumonoganakereba
-        if(divide_function::search(t)||Tree::depth_of_cur_is_max(depth)){//entropy 0 or depth max
-          if(divide_function::search(t))cout<<"search"<<endl;
-          if(Tree::depth_of_cur_is_max(depth))cout<<"depthmax"<<endl;
-          for(i=0;i<16;i++){
-            if(Tree::check_pwd(i)){
-              Tree::end(i);
+    for(int j=0;j<t;j++){//T個のサブセットについて
+      while(Tree::next_exist(j)){//末端まで行っているものがなければ、まだやる必要があるので
+        if(divide_function::search(j)||Tree::depth_of_cur_is_max(j, depth)){//entropy 0 or depth max
+          if(divide_function::search(j))cout<<"search"<<endl;
+          if(Tree::depth_of_cur_is_max(j, depth))cout<<"depthmax"<<endl;
+          for(int i=0;i<16;i++){
+            if(Tree::check_pwd(j, i)){
+              Tree::end(j, i);
             }
           }
-          Tree::next();
+          Tree::next(j);
         }
         else{
-          divide_function::check(K, ty);
-          Tree::next();
+          divide_function::check(j, K, ty);
+          cout<<"point j:"<<j<<endl;
+          Tree::next(j);
+          cout<<"learn next called"<<endl;
         }
       }
     }
   }
-  void check(int K, int ty){
+  void check(int t,int K, int ty){
+    //引数　t:t個目のサブセット K:関数候補の数 ty:識別関数のタイプ
     r=(double*)malloc(sizeof(double)*K);
     I=(double*)malloc(sizeof(double)*K);
     switch (ty){//ty候補のタイプ
     case 1:       //候補タイプ1 変数の大きさについて単純にしきい値と比較
-      for (i=0;i<K;i++){
+      for (int i=0;i<K;i++){
         cout<<"K:"<<i<<"  "<<endl;
         left=0; right=0;
         Hl=0;Hr=0;
-        for(j=0;j<3;j++){
+        for(int j=0;j<3;j++){
           left_[i]=0;
           right_[i]=0;
         }
@@ -259,10 +372,10 @@ public:
         r[i]=rand()%6+4;
         cout<<"r["<<i<<"]:"<<r[i]<<endl;
 
-        for (j=0;j<16;j++){
+        for (int j=0;j<16;j++){
           cout<<"    "<<j<<"bannme"<<endl;
-          if(Tree::check_pwd(j)){//データ１６個の中で現在のディレクトリにあるものについて
-            cout<<"curr directory:"<<Tree::curr()<<"    ";
+          if(Tree::check_pwd(t, j)){//データ１６個の中で現在のディレクトリにあるものについて
+            cout<<"curr["<<t<<"] directory:"<<Tree::curr(t)<<"    ";
 
             //cout<<"a"<<endl;
             if(Set::s_out(0,t,j)>r[i]){
@@ -308,7 +421,7 @@ public:
           }
         }
         cout<<"H"<<endl;
-        for(j=0;j<3;j++){//H(Sl)の計算
+        for(int j=0;j<3;j++){//H(Sl)の計算
           if(left_[j]==0){cout<<"left_["<<j<<"]:0"<<endl;}
           else{
             cout<<"left_["<<j<<"]"<<left_[j];
@@ -321,7 +434,7 @@ public:
             //  cout<<"tmp:"<<tmp<<endl;
           }
         }
-        for(j=0;j<3;j++){//H(Sr)の計算
+        for(int j=0;j<3;j++){//H(Sr)の計算
           if(right_[j]==0){cout<<"right_["<<j<<"]:0"<<endl;}
           else{
             cout<<"right_["<<j<<"]:"<<right_[j];
@@ -344,15 +457,15 @@ public:
       }
       //Kkononakade最大となるrで分岐
       cout<<"r:"<<r[max]<<endl;
-      for(j=0;j<16;j++){
-        if(Tree::check_pwd(j)){//データ１６個の中で現在のディレクトリにあるものがあれば
+      for(int j=0;j<16;j++){
+        if(Tree::check_pwd(t, j)){//データ１６個の中で現在のディレクトリにあるものがあれば
           if(Set::s_out(0,t,j)>r[max]){
-            Tree::left(j);
-            cout<<"Tree::left("<<j<<")   "<<"Set::sa["<<t<<"]["<<j<<"]:"<<Set::s_out(0,t,j)<<endl<<endl;
+            Tree::left(t, j);
+            cout<<"最大となるrで分岐:::::Tree::left("<<t<<""<<j<<")   "<<"Set::sa["<<t<<"]["<<j<<"]:"<<Set::s_out(0,t,j)<<endl<<endl;
           }
           else{
-            Tree::right(j);
-            cout<<"Tree::right("<<j<<")  "<<"Set::sa["<<t<<"]["<<j<<"]:"<<Set::s_out(0,t,j)<<endl<<endl;
+            Tree::right(t, j);
+            cout<<"最大となるrで分岐:::::Tree::right("<<t<<""<<j<<")  "<<"Set::sa["<<t<<"]["<<j<<"]:"<<Set::s_out(0,t,j)<<endl<<endl;
           }
         }
       }
@@ -372,7 +485,7 @@ int main (int argc, char *argv[]){
     cerr << "File do not exist.\n";
     return 0;
   }
-
+  int i=0;
   while(getline(ifs, str)) {
     a[i]=0; b[i]=0; c[i]=0,d[i]=0;
     sscanf(str.data(), "%lf%lf%lf%lf%d", &a[i], &b[i], &c[i], &d[i], &e[i]);
@@ -385,13 +498,13 @@ int main (int argc, char *argv[]){
     /*cout << i<< endl;*/
     i++;
   }
-  
+  i=0;
   //サブセット作成 １セットに含まれるデータは16とする
   
   //Set s;
   divide_function s;
-  for (i=0;i<T;i++){//T個のサブセットについて
-    for (j=0;j<16;j++){
+  for (int i=0;i<T;i++){//T個のサブセットについて
+    for (int j=0;j<16;j++){
       s.in(i, j, rand()%150);
       //cout << rand()%150<<endl;
       //cout<< "i"<<i<<"   j"<<j<<endl;
@@ -421,7 +534,7 @@ int main (int argc, char *argv[]){
   //s.next();  s.next();  s.next();
 
   s.learn(3, 1, 1, 1);
-  s.state();
+  s.state(0);
   return 0;
 }
 
